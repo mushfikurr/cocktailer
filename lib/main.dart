77c8bd3817b2db102003a1de97e03fa0665047fe
Mushfikur Rahman <mushfikur0@gmail.com>
2023-11-04 03:13:31 +0000

    Restructure lib folder.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/colors.dart';
-import 'package:flutter_application_1/individual_cocktail.dart';
+import 'package:flutter_application_1/view_individual_cocktail.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
