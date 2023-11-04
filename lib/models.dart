b794e0f1fe57142bda2d0cf7315532fd3403a05a
Mushfikur Rahman <mushfikur0@gmail.com>
2023-11-04 01:18:30 +0000

    Serialise and helper func for all ingredients

--- lib/models.dart
+++ lib/models.dart
@@ -12,10 +12,32 @@ class Drink {
   String? strIngredient2;
   String? strIngredient3;
   String? strIngredient4;
+  String? strIngredient5;
+  String? strIngredient6;
+  String? strIngredient7;
+  String? strIngredient8;
+  String? strIngredient9;
+  String? strIngredient10;
+  String? strIngredient11;
+  String? strIngredient12;
+  String? strIngredient13;
+  String? strIngredient14;
+  String? strIngredient15;
   String? strMeasure1;
   String? strMeasure2;
   String? strMeasure3;
   String? strMeasure4;
+  String? strMeasure5;
+  String? strMeasure6;
+  String? strMeasure7;
+  String? strMeasure8;
+  String? strMeasure9;
+  String? strMeasure10;
+  String? strMeasure11;
+  String? strMeasure12;
+  String? strMeasure13;
+  String? strMeasure14;
+  String? strMeasure15;
   String? strTags;
   String? strVideo;
   String? strIBA;
@@ -29,7 +51,7 @@ class Drink {
   String? strImageAttribution;
   String? strCreativeCommonsConfirmed;
   String? dateModified;
-  String? strDrinkAlternate; // New optional field
+  String? strDrinkAlternate;
 
   Drink({
     required this.idDrink,
@@ -43,10 +65,32 @@ class Drink {
     this.strIngredient2,
     this.strIngredient3,
     this.strIngredient4,
+    this.strIngredient5,
+    this.strIngredient6,
+    this.strIngredient7,
+    this.strIngredient8,
+    this.strIngredient9,
+    this.strIngredient10,
+    this.strIngredient11,
+    this.strIngredient12,
+    this.strIngredient13,
+    this.strIngredient14,
+    this.strIngredient15,
     this.strMeasure1,
     this.strMeasure2,
     this.strMeasure3,
     this.strMeasure4,
+    this.strMeasure5,
+    this.strMeasure6,
+    this.strMeasure7,
+    this.strMeasure8,
+    this.strMeasure9,
+    this.strMeasure10,
+    this.strMeasure11,
+    this.strMeasure12,
+    this.strMeasure13,
+    this.strMeasure14,
+    this.strMeasure15,
     this.strTags,
     this.strVideo,
     this.strIBA,
@@ -76,10 +120,32 @@ class Drink {
       'strIngredient2': strIngredient2,
       'strIngredient3': strIngredient3,
       'strIngredient4': strIngredient4,
+      'strIngredient5': strIngredient5,
+      'strIngredient6': strIngredient6,
+      'strIngredient7': strIngredient7,
+      'strIngredient8': strIngredient8,
+      'strIngredient9': strIngredient9,
+      'strIngredient10': strIngredient10,
+      'strIngredient11': strIngredient11,
+      'strIngredient12': strIngredient12,
+      'strIngredient13': strIngredient13,
+      'strIngredient14': strIngredient14,
+      'strIngredient15': strIngredient15,
       'strMeasure1': strMeasure1,
       'strMeasure2': strMeasure2,
       'strMeasure3': strMeasure3,
       'strMeasure4': strMeasure4,
+      'strMeasure5': strMeasure5,
+      'strMeasure6': strMeasure6,
+      'strMeasure7': strMeasure7,
+      'strMeasure8': strMeasure8,
+      'strMeasure9': strMeasure9,
+      'strMeasure10': strMeasure10,
+      'strMeasure11': strMeasure11,
+      'strMeasure12': strMeasure12,
+      'strMeasure13': strMeasure13,
+      'strMeasure14': strMeasure14,
+      'strMeasure15': strMeasure15,
       'strTags': strTags,
       'strVideo': strVideo,
       'strIBA': strIBA,
@@ -110,10 +176,32 @@ class Drink {
       strIngredient2: json['strIngredient2'],
       strIngredient3: json['strIngredient3'],
       strIngredient4: json['strIngredient4'],
+      strIngredient5: json['strIngredient5'],
+      strIngredient6: json['strIngredient6'],
+      strIngredient7: json['strIngredient7'],
+      strIngredient8: json['strIngredient8'],
+      strIngredient9: json['strIngredient9'],
+      strIngredient10: json['strIngredient10'],
+      strIngredient11: json['strIngredient11'],
+      strIngredient12: json['strIngredient12'],
+      strIngredient13: json['strIngredient13'],
+      strIngredient14: json['strIngredient14'],
+      strIngredient15: json['strIngredient15'],
       strMeasure1: json['strMeasure1'],
       strMeasure2: json['strMeasure2'],
       strMeasure3: json['strMeasure3'],
       strMeasure4: json['strMeasure4'],
+      strMeasure5: json['strMeasure5'],
+      strMeasure6: json['strMeasure6'],
+      strMeasure7: json['strMeasure7'],
+      strMeasure8: json['strMeasure8'],
+      strMeasure9: json['strMeasure9'],
+      strMeasure10: json['strMeasure10'],
+      strMeasure11: json['strMeasure11'],
+      strMeasure12: json['strMeasure12'],
+      strMeasure13: json['strMeasure13'],
+      strMeasure14: json['strMeasure14'],
+      strMeasure15: json['strMeasure15'],
       strTags: json['strTags'],
       strVideo: json['strVideo'],
       strIBA: json['strIBA'],
@@ -130,4 +218,20 @@ class Drink {
       strDrinkAlternate: json['strDrinkAlternate'],
     );
   }
+}
+
+List<(String, String)> getIngredientsAsList(Drink drink) {
+  List<(String, String)> ingredients = [];
+  for (int ingredientNumber = 1; ingredientNumber < 16; ingredientNumber++) {
+    String ingredientProperty = "strIngredient$ingredientNumber";
+    if (drink.toJson()[ingredientProperty] != null) {
+      String measureProperty = "strMeasure$ingredientNumber";
+      ingredients.add((
+        drink.toJson()[ingredientProperty],
+        drink.toJson()[measureProperty]
+      ));
+    }
+  }
+
+  return ingredients;
 }
